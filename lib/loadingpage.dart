658c5cf18b8a9e176dab7e82e778fdc1eee85c7b
viniciosm730 <viniciosm730@gmail.com>
2025-10-29 22:14:41 +0000

    1º First Commit

---
+++ lib/loadingpage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LoadingAnimationWidget.flickr(
+          leftDotColor: const Color(0xFF1A1A3F),
+          rightDotColor: const Color(0xFFEA3799),
+          size: 50,
+        ),
+      ),
+    );
+  }
+}
+
